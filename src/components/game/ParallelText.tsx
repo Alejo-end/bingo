cdec761ae4f1bab124c1b78a97901498322ca4e5
Alejo-end <alejandroe.deleon@gmail.com>
2023-03-07 08:26:46 +0100

    adding parallelText for background animation

---
+++ src/components/game/ParallelText.tsx
@@ -0,0 +1,78 @@
+
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame
+} from "framer-motion";
+import { wrap } from "@motionone/utils";
+
+interface ParallaxProps {
+  children: string;
+  baseVelocity: number;
+}
+
+function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 3000], [0, 85], {
+    clamp: false
+  });
+
+  /**
+   * This is a magic wrapping for the length of the text - you
+   * have to replace for wrapping that works for you or dynamically
+   * calculate
+   */
+  const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((_t: DOMHighResTimeStamp) => {
+    let moveBy = directionFactor.current * baseVelocity * (_t / 800);
+  
+    /**
+     * This is what changes the direction of the scroll once we
+     * switch scrolling directions.
+     */
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -2;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 0.6;
+    }
+  
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+  
+    baseX.set(baseX.get() + moveBy);
+  });
+  
+
+  /**
+   * The number of times to repeat the child text should be dynamically calculated
+   * based on the size of the text and viewport. Likewise, the x motion value is
+   * currently wrapped between -20 and -45% - this 25% is derived from the fact
+   * we have four children (100% / 4). This would also want deriving from the
+   * dynamically generated number of children.
+   */
+  return (
+    <div className="parallax">
+      <motion.div className="scroller" style={{ x }}>
+        <h1>{children}</h1>
+        <h1>{children}</h1>
+        <h1>{children}</h1>
+        <h1>{children}</h1>
+      </motion.div>
+    </div>
+  );
+}
+
+
+export default ParallaxText;
